f4e279ef608137896751b887dd8bfd7f6453fc89
James Reed <jamesreed@fb.com>
2022-04-04 01:10:35 +0000

    Fix handling of starred arguments when invoking pipe with kwargs

--- pippy/IR.py
+++ pippy/IR.py
@@ -254,7 +254,15 @@ class Pipe(torch.nn.Module):
                     if node.args and len(node.args) > 0:
                         parameters.append(Parameter(node.target, Parameter.POSITIONAL_OR_KEYWORD, default=node.args[0]))
                     else:
-                        parameters.append(Parameter(node.target, Parameter.POSITIONAL_OR_KEYWORD))
+                        parameter_kind = Parameter.POSITIONAL_OR_KEYWORD
+                        param_name = node.target
+                        if node.target.startswith('**'):
+                            parameter_kind = Parameter.VAR_KEYWORD
+                            param_name = param_name[2:]
+                        elif node.target.startswith('*'):
+                            parameter_kind = Parameter.VAR_POSITIONAL
+                            param_name = param_name[1:]
+                        parameters.append(Parameter(param_name, parameter_kind))
             signature = Signature(parameters)
             ba = signature.bind(*args, **kwargs)
             ba.apply_defaults()
