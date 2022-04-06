a9db0c6d2d9d23585fc94bc54876bb4844978ae3
James Reed <jamesreed@fb.com>
2022-04-06 21:02:24 +0000

    Switch integration tests to use test matrix & enable schedule for HF integration tests

--- test/launch_local_test_forward_hf_gpt2.sh
+++ test/launch_local_test_forward_hf_gpt2.sh
@@ -1,4 +1,4 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
-torchrun --standalone --nnodes=1 --nproc_per_node=15 "${SCRIPTPATH}/local_test_forward_hf_gpt2.py"
+torchrun --standalone --nnodes=1 --nproc_per_node=15 "${SCRIPTPATH}/local_test_forward_hf_bert.py" $@
