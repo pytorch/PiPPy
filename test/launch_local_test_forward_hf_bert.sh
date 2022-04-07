437675db8f8de865e84c45cb83d4e0884b314baa
James Reed <jamesreed@fb.com>
2022-04-06 17:17:34 -0700

    Switch integration tests to use test matrix & enable schedule for HF … (#96)
    * Switch integration tests to use test matrix & enable schedule for HF integration tests
    
    * delete launch scripts
    
    * fix typo
    
    * Apply suggestions from code review
    
    Co-authored-by: Pavel Belevich <pbelevich@fb.com>
    
    Co-authored-by: Pavel Belevich <pbelevich@fb.com>

--- test/launch_local_test_forward_hf_bert.sh
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-torchrun --standalone --nnodes=1 --nproc_per_node=15 "${SCRIPTPATH}/local_test_forward_hf_bert.py"
