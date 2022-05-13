3b446c92eff6ffc69fe0a708afd5f74e0fac7012
Ke Wen <kw2501@fb.com>
2022-05-13 15:21:28 -0700

    Use python3 and pip3

--- .github/workflows/check_docker_env.sh
+++ .github/workflows/check_docker_env.sh
@@ -2,10 +2,11 @@
 
 nvidia-smi
 nvcc --version
-which python
-python --version
-which pip
-pip --version
+which python3
+python3 --version
+yum install -y python3-pip
+which pip3
+pip3 --version
 
 # Print test options
 echo "VERBOSE: ${VERBOSE}"
@@ -13,20 +14,20 @@ echo "REPLICATE: ${REPLICATE}"
 echo "SCHEDULE: ${SCHEDULE}"
 
 # Install dependencies
-pip install flake8 pytest pytest-cov numpy
-if [ -f requirements.txt ]; then pip install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html; fi
+pip3 install flake8 pytest pytest-cov numpy
+if [ -f requirements.txt ]; then pip3 install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html; fi
 
 # Install pavel's huggingface fork
-pip install git+https://github.com/pbelevich/transformers.git@compatible_with_pt_master sentencepiece
+pip3 install git+https://github.com/pbelevich/transformers.git@compatible_with_pt_master sentencepiece
 
 # Install pippy
-python setup.py install
+python3 setup.py install
 
 # Run all integration tests
-python test/local_test_forward.py --replicate ${REPLICATE} -s ${SCHEDULE}
-python test/local_test_forward_backward.py --replicate ${REPLICATE} -s ${SCHEDULE}
-python examples/slurm/hf/gpt2/pippy_gpt2.py --replicate ${REPLICATE} -s ${SCHEDULE}
+python3 test/local_test_forward.py --replicate ${REPLICATE} -s ${SCHEDULE}
+python3 test/local_test_forward_backward.py --replicate ${REPLICATE} -s ${SCHEDULE}
+python3 examples/slurm/hf/gpt2/pippy_gpt2.py --replicate ${REPLICATE} -s ${SCHEDULE}
 
 # Run flaky integration tests
-python test/local_test_forward_hf_gpt2.py --replicate ${REPLICATE} -s ${SCHEDULE} || true
-python test/local_test_forward_hf_bert.py --replicate ${REPLICATE} -s ${SCHEDULE} || true
+python3 test/local_test_forward_hf_gpt2.py --replicate ${REPLICATE} -s ${SCHEDULE} || true
+python3 test/local_test_forward_hf_bert.py --replicate ${REPLICATE} -s ${SCHEDULE} || true
