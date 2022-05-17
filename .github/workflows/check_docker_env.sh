09d92a568f89a129505ddf13fcf63214a2d47cfd
Ke Wen <kw2501@fb.com>
2022-05-17 20:25:45 +0000

    Use CUDA+PyThon image

--- .github/workflows/check_docker_env.sh
+++ .github/workflows/check_docker_env.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-set -ex
-
-nvidia-smi
-nvcc --version
-which python3
-python3 --version
-yum install -y python3-pip
-which pip3
-pip3 --version
+set -x
 
 # Print test options
 echo "VERBOSE: ${VERBOSE}"
 echo "REPLICATE: ${REPLICATE}"
 echo "SCHEDULE: ${SCHEDULE}"
 
+nvidia-smi
+nvcc --version
+which python3
+python3 --version
+which pip3
+pip3 --version
+
+# Install git
+apt-get update
+apt-get install git -y
+
 # Install dependencies
-# Turn off progress bar to save logs (but got error unknown command "config")
-#pip3 config set global.progress_bar off
-pip3 install flake8 pytest pytest-cov numpy --progress-bar off
-if [ -f requirements.txt ]; then pip3 install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html --progress-bar off; fi
+# Turn off progress bar to save logs
+pip3 config set global.progress_bar off
+pip3 install flake8 pytest pytest-cov numpy
+if [ -f requirements.txt ]; then pip3 install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html; fi
 
 # Install pavel's huggingface fork
-pip3 install git+https://github.com/pbelevich/transformers.git@compatible_with_pt_master sentencepiece --progress-bar off
+pip3 install git+https://github.com/pbelevich/transformers.git@compatible_with_pt_master sentencepiece
 
 # Install pippy
 python3 setup.py install
