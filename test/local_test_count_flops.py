ebe66b64b2ecc5b0132ac008ed1057270abfe222
max <maksim.levental@gmail.com>
2022-07-18 21:39:47 -0500

    Add Albert example (substantive refactor).

--- test/local_test_count_flops.py
+++ test/local_test_count_flops.py
@@ -1,6 +1,12 @@
-import torch
+import inspect
+from pprint import pprint
 
-from pippy.count_flops import compile_model_op_by_op, count_flop_latency_in_mlir_module
+import torch
+import transformers.utils.fx as fx
+from torch.fx.passes.shape_prop import ShapeProp
+from transformers import AlbertModel, AlbertConfig
+
+from pippy.count_flops import compile_model_op_by_op, count_flop_latency_in_mlir_modules
 
 
 class ExampleCode(torch.nn.Module):
@@ -25,18 +31,72 @@ class ExampleCode(torch.nn.Module):
         return x
 
 
-def main():
+@torch.fx.wrap
+def torch_ones_wrapper(*args, **kwargs):
+    return torch.ones(*args, **kwargs)
+
+
+class HFBertTracer(fx.HFTracer):
+    def trace(self, root, concrete_args=None):
+        graph = super().trace(root, concrete_args)
+        # HACK to replace HF's non-resolvable wrappers with the original
+        # tensor constructor functions
+        # Requires this patch to HF: https://github.com/jamesr66a/transformers/commit/ede9d30f36f12be390692617ef76e2928b1612bd
+        for node in graph.nodes:
+            if node.op == "call_function":
+                if getattr(node.target, "_orig", None) == torch.ones:
+                    node.target = torch_ones_wrapper
+        return graph
+
+
+torch.fx.Tracer.proxy_buffer_attributes = True
+
+
+def small_test():
     d_hid = 10
     bs = 4
     model = ExampleCode(d_hid, bs)
     x = torch.randn(bs, d_hid)
 
-    mlir_modules = compile_model_op_by_op(model, x)
+    node_mlir_module_strs = compile_model_op_by_op(model, x)
+    latencies = count_flop_latency_in_mlir_modules(node_mlir_module_strs)
+    pprint(latencies)
 
-    for submod_name, main_op_name, mlir_module in mlir_modules:
-        total_latency = count_flop_latency_in_mlir_module(mlir_module)
-        print(main_op_name, total_latency)
+
+def trace_albert(albert):
+    input_names = albert.dummy_inputs.keys()
+    sig = inspect.signature(albert.forward)
+    concrete_args = {
+        p.name: p.default for p in sig.parameters.values() if p.name not in input_names
+    }
+
+    hf_tracer = HFBertTracer()
+    graph = hf_tracer.trace(albert, concrete_args=concrete_args)
+    traced = torch.fx.GraphModule(albert, graph)
+    traced.graph.eliminate_dead_code()
+
+    return traced
+
+
+def albert_test():
+    bs = 1
+    seq_length = 2
+
+    albert = AlbertModel(AlbertConfig(hidden_size=128, intermediate_size=128))
+    albert.eval()
+
+    albert_input = torch.zeros(bs, seq_length, dtype=torch.long).random_(
+        albert.config.vocab_size
+    )
+    albert(albert_input)
+
+    node_mlir_modules = compile_model_op_by_op(
+        albert, albert_input, tracer=trace_albert
+    )
+    latencies = count_flop_latency_in_mlir_modules(node_mlir_modules)
+    pprint(latencies)
 
 
 if __name__ == "__main__":
-    main()
+    small_test()
+    albert_test()
