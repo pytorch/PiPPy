168024edcc3dfa81177c4308635b82bd7fc9c2a4
James Reed <jamesreed@fb.com>
2022-07-20 21:02:32 +0000

    unittest.TestCase entrypoints for integration tests

--- test/local_test_forward.py
+++ test/local_test_forward.py
@@ -4,6 +4,7 @@ import logging
 import os
 import socket
 from typing import Dict
+import unittest
 
 import torch
 import torch.distributed.rpc as rpc
@@ -139,7 +140,7 @@ def run_worker(rank, world_size, args):
     rpc.shutdown()
 
 
-if __name__ == "__main__":
+def main(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--world_size', type=int, default=int(os.getenv("WORLD_SIZE", 4)))
     parser.add_argument('--rank', type=int, default=int(os.getenv("RANK", -1)))
@@ -150,7 +151,7 @@ if __name__ == "__main__":
     parser.add_argument('--cuda', type=int, default=int(torch.cuda.is_available()))
     parser.add_argument('--record_mem_dumps', type=int, default=0, choices=[0, 1])
     parser.add_argument('--checkpoint', type=int, default=0, choices=[0, 1])
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
     # Interleaved 1F1B uses less ranks than number of stages
     if args.schedule == 'Interleaved1F1B':
@@ -162,3 +163,15 @@ if __name__ == "__main__":
         run_worker(args.rank, args.world_size, args)
     else:
         print("I'm unused, exiting")
+
+
+if __name__ == "__main__":
+    main()
+
+class LocalTestForwardTest(unittest.TestCase):
+    def test_forward(self):
+        import random
+        port = random.randint(29500, 30000)
+        args = ['--cuda', os.getenv('USE_CUDA', '0'),
+                '--master_port', str(port)]
+        main(args)
