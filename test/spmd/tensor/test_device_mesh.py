a7ca3535ad2a685fd8da2c2416436e3ec311e947
Yushuang Luo <ysluo@fb.com>
2022-08-04 17:18:05 -0700

    update multi-d tests to compute subgroup ranks by itself.

--- test/spmd/tensor/test_device_mesh.py
+++ test/spmd/tensor/test_device_mesh.py
@@ -263,17 +263,16 @@ class DeviceMeshCollectiveTest(DistTensorTestBase):
         mesh_tensor = torch.arange(8).reshape(2, 2, 2)
         mesh = DeviceMesh(self.device_type, mesh_tensor)
         local_tensor = torch.ones(3, 3) * self.rank
-        expected_ranks_by_dim = [
-            [[0, 4], [2, 6], [1, 5], [3, 7]],
-            [[0, 2], [1, 3], [4, 6], [5, 7]],
-            [[0, 1], [2, 3], [4, 5], [6, 7]],
-        ]
-        for dim, dim_ranks in enumerate(expected_ranks_by_dim):
-            for ranks in dim_ranks:
-                if self.rank in ranks:
-                    reduced_tensor = mesh.all_reduce(local_tensor, mesh_dim=dim)
-                    res_num = sum(ranks)
-                    break
+
+        # check all dim groups
+        dim_to_subgroups = mesh.get_dim_groups()
+        for dim, dim_group in enumerate(dim_to_subgroups):
+            dim_group_size = get_world_size(dim_group)
+            global_ranks = [
+                _get_global_rank(dim_group, i) for i in range(dim_group_size)
+            ]
+            reduced_tensor = mesh.all_reduce(local_tensor, mesh_dim=dim)
+            res_num = sum(global_ranks)
             self.assertEqual(reduced_tensor, torch.ones(3, 3) * res_num)
 
     @with_comms
@@ -281,17 +280,16 @@ class DeviceMeshCollectiveTest(DistTensorTestBase):
         mesh_tensor = torch.arange(8).reshape(2, 2, 2)
         mesh = DeviceMesh(self.device_type, mesh_tensor)
         local_tensor = torch.ones(3, 3) * self.rank
-        expected_ranks_by_dim = [
-            [[0, 4], [2, 6], [1, 5], [3, 7]],
-            [[0, 2], [1, 3], [4, 6], [5, 7]],
-            [[0, 1], [2, 3], [4, 5], [6, 7]],
-        ]
-        for dim, dim_ranks in enumerate(expected_ranks_by_dim):
-            for ranks in dim_ranks:
-                if self.rank in ranks:
-                    received_tensor = mesh.broadcast(local_tensor, mesh_dim=dim)
-                    res_num = ranks[0]
-                    break
+
+        # check all dim groups
+        dim_to_subgroups = mesh.get_dim_groups()
+        for dim, dim_group in enumerate(dim_to_subgroups):
+            dim_group_size = get_world_size(dim_group)
+            global_ranks = [
+                _get_global_rank(dim_group, i) for i in range(dim_group_size)
+            ]
+            received_tensor = mesh.broadcast(local_tensor, mesh_dim=dim)
+            res_num = global_ranks[0]
             self.assertEqual(received_tensor, torch.ones(3, 3) * res_num)
 
     @with_comms
@@ -299,21 +297,17 @@ class DeviceMeshCollectiveTest(DistTensorTestBase):
         mesh_tensor = torch.arange(8).reshape(2, 2, 2)
         mesh = DeviceMesh(self.device_type, mesh_tensor)
 
-        expected_ranks_by_dim = [
-            [[0, 4], [2, 6], [1, 5], [3, 7]],
-            [[0, 2], [1, 3], [4, 6], [5, 7]],
-            [[0, 1], [2, 3], [4, 5], [6, 7]],
-        ]
-        for dim, dim_ranks in enumerate(expected_ranks_by_dim):
-            for ranks in dim_ranks:
-                scattered_tensors = [
-                    torch.ones(3, 3) * global_rank for global_rank in ranks
-                ]
-                if self.rank in ranks:
-                    received_tensor = mesh.scatter(
-                        scattered_tensors, mesh_dim=dim
-                    )
-                    break
+        # check all dim groups
+        dim_to_subgroups = mesh.get_dim_groups()
+        for dim, dim_group in enumerate(dim_to_subgroups):
+            dim_group_size = get_world_size(dim_group)
+            global_ranks = [
+                _get_global_rank(dim_group, i) for i in range(dim_group_size)
+            ]
+            scattered_tensors = [
+                torch.ones(3, 3) * global_rank for global_rank in global_ranks
+            ]
+            received_tensor = mesh.scatter(scattered_tensors, mesh_dim=dim)
             self.assertEqual(received_tensor, torch.ones(3, 3) * self.rank)
 
 
