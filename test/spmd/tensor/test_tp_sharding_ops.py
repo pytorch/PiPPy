acd047eb054c9ecc6a5714be90246fc5122361a7
fduwjj <fduwjj@fb.com>
2022-08-10 21:58:27 +0000

    revert test chage

--- test/spmd/tensor/test_tp_sharding_ops.py
+++ test/spmd/tensor/test_tp_sharding_ops.py
@@ -15,10 +15,8 @@ class TPShardingOpsTest(DistTensorTestBase):
         tensor = torch.rand(16, 35, 26)
         sharding = [Shard(0)]
         st = distribute_tensor(tensor, device_mesh, sharding).view(8, 4, 35, 13)
-        st_new = distribute_tensor(
-            tensor.view(8, 4, 35, 13), device_mesh, sharding
-        )
-        torch.testing.assert_close(st.to_local(), st_new.to_local())
+        st_new = distribute_tensor(tensor.view(8, 4, 35, 13), device_mesh, sharding)
+        self.assertEqual(st.to_local(), st_new.to_local())
         self.assertEqual(st.placements[0], st_new.placements[0])
 
     @with_comms
@@ -30,10 +28,10 @@ class TPShardingOpsTest(DistTensorTestBase):
         dist_tensor = DTensor.from_local(tensor, device_mesh, sharding)
         new_dt = dist_tensor.transpose(0, 2)
         self.assertTrue(new_dt.placements[0].is_shard(dim=2))
-        torch.testing.assert_close(new_dt.to_local(), tensor.transpose(0, 2))
+        self.assertEqual(new_dt.to_local(), tensor.transpose(0, 2))
         new_dt = dist_tensor.transpose(1, 2)
         self.assertTrue(new_dt.placements[0].is_shard(dim=0))
-        torch.testing.assert_close(new_dt.to_local(), tensor.transpose(1, 2))
+        self.assertEqual(new_dt.to_local(), tensor.transpose(1, 2))
 
     # TODO: Need to investigate why test failed in CPU for baddbmm.
     @with_comms
@@ -44,18 +42,14 @@ class TPShardingOpsTest(DistTensorTestBase):
         tensor = torch.rand(3, 5, 6, device=self.device_type)
         batch_1 = torch.rand(3, 5, 8, device=self.device_type)
         batch_2 = torch.rand(3, 8, 6, device=self.device_type)
-        local_result = torch.baddbmm(
-            tensor, batch_1, batch_2, beta=0.0, alpha=0.5
-        )
+        local_result = torch.baddbmm(tensor, batch_1, batch_2, beta=0.0, alpha=0.5)
         sharding = [Shard(0)]
         tensor_dt = DTensor.from_local(tensor, device_mesh, sharding)
         batch_1_dt = DTensor.from_local(batch_1, device_mesh, sharding)
         batch_2_dt = DTensor.from_local(batch_2, device_mesh, sharding)
-        new_dt = torch.baddbmm(
-            tensor_dt, batch_1_dt, batch_2_dt, beta=0.0, alpha=0.5
-        )
+        new_dt = torch.baddbmm(tensor_dt, batch_1_dt, batch_2_dt, beta=0.0, alpha=0.5)
         self.assertTrue(new_dt.placements[0].is_shard(dim=0))
-        torch.testing.assert_close(new_dt.to_local(), local_result)
+        self.assertEqual(new_dt.to_local(), local_result)
 
     @with_comms
     def test_sharded_bmm(self):
@@ -69,14 +63,12 @@ class TPShardingOpsTest(DistTensorTestBase):
         mat_2_dt = DTensor.from_local(mat_2, device_mesh, sharding)
         new_dt = torch.bmm(input_dt, mat_2_dt)
         self.assertTrue(new_dt.placements[0].is_shard(dim=0))
-        torch.testing.assert_close(new_dt.to_local(), local_result)
+        self.assertEqual(new_dt.to_local(), local_result)
 
     @with_comms
     def test_sharded_softmax(self):
         for softmax_dim in [1, 2, -1]:
-            device_mesh = DeviceMesh(
-                self.device_type, list(range(self.world_size))
-            )
+            device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
             torch.manual_seed(self.rank)
             input = torch.rand(15, 27, 16, device=self.device_type)
             local_result = torch.nn.functional.softmax(
@@ -88,7 +80,7 @@ class TPShardingOpsTest(DistTensorTestBase):
                 input_dt, dim=softmax_dim, dtype=torch.float32
             )
             self.assertTrue(new_dt.placements[0].is_shard(dim=0))
-            torch.testing.assert_close(new_dt.to_local(), local_result)
+            self.assertEqual(new_dt.to_local(), local_result)
 
     @with_comms
     def test_sharded_permute(self):
@@ -99,7 +91,7 @@ class TPShardingOpsTest(DistTensorTestBase):
         dist_tensor = DTensor.from_local(tensor, device_mesh, sharding)
         new_dt = dist_tensor.permute(1, 0, 2)
         self.assertTrue(new_dt.placements[0].is_shard(dim=1))
-        torch.testing.assert_close(new_dt.to_local(), tensor.permute(1, 0, 2))
+        self.assertEqual(new_dt.to_local(), tensor.permute(1, 0, 2))
 
     @with_comms
     def test_replicated_permute(self):
@@ -110,10 +102,8 @@ class TPShardingOpsTest(DistTensorTestBase):
         dist_tensor = DTensor.from_local(tensor, device_mesh, sharding)
         new_dt = dist_tensor.permute(1, 0, 2)
         self.assertTrue(new_dt.placements[0].is_replicate())
-        torch.testing.assert_close(new_dt.to_local(), tensor.permute(1, 0, 2))
-        torch.testing.assert_close(
-            new_dt.stride(), tensor.permute(1, 0, 2).stride()
-        )
+        self.assertEqual(new_dt.to_local(), tensor.permute(1, 0, 2))
+        self.assertEqual(new_dt.stride(), tensor.permute(1, 0, 2).stride())
 
     @with_comms
     def test_sharded_cat(self):
@@ -130,8 +120,8 @@ class TPShardingOpsTest(DistTensorTestBase):
         cat_dt = DTensor.from_local(
             torch.cat([tensor_1, tensor_2, tensor_3]), device_mesh, sharding
         )
-        torch.testing.assert_close(new_dt.to_local(), cat_dt.to_local())
-        torch.testing.assert_close(new_dt.size(), cat_dt.size())
+        self.assertEqual(new_dt.to_local(), cat_dt.to_local())
+        self.assertEqual(new_dt.size(), cat_dt.size())
 
     @with_comms
     def test_sharded_split(self):
@@ -144,7 +134,7 @@ class TPShardingOpsTest(DistTensorTestBase):
         local_tensors = tensor.split(3, dim=-1)
         for idx, dt in enumerate(dt_list):
             self.assertTrue(dt.placements[0].is_shard(dim=2))
-            torch.testing.assert_close(dt.to_local(), local_tensors[idx])
+            self.assertEqual(dt.to_local(), local_tensors[idx])
 
     @with_comms
     def test_view_with_sharding_dim_change(self):
@@ -155,7 +145,7 @@ class TPShardingOpsTest(DistTensorTestBase):
         dt = DTensor.from_local(tensor, device_mesh, sharding)
         dt = dt._view_with_sharding_dim_change(1, (3, -1, 6))
         self.assertTrue(dt.placements[0].is_shard(dim=1))
-        torch.testing.assert_close(dt.to_local(), tensor.view(3, -1, 6))
+        self.assertEqual(dt.to_local(), tensor.view(3, -1, 6))
 
 
 if __name__ == "__main__":
