0d95fb3ffdb26355f863645b8af4cd8455f5130a
Pavel Belevich <pbelevich@fb.com>
2022-08-10 12:13:06 -0400

    formatting

--- pippy/backward.py
+++ pippy/backward.py
@@ -4,18 +4,21 @@ from typing import List
 import torch
 import pippy.fx
 
-def stage_backward(stage_output, output_grads, input_values, stage_info : str, outputs_with_grads_idxs : List[int]):
+
+def stage_backward(stage_output, output_grads, input_values, stage_info: str, outputs_with_grads_idxs: List[int]):
     """
     Given the input value(s) and the corresponding gradient for those/that input
     value(s), compute and accumulate gradients for all parameter values (leaves
     in the autograd trace) as well as return a list of the gradients for the
     input values
     """
+
     def friendly_debug_info(v):
         if isinstance(v, torch.Tensor):
             return f'Tensor(size={v.shape})'
         else:
             return str(v)
+
     try:
         stage_output_with_grads = [stage_output[i] for i in outputs_with_grads_idxs]
         output_grads_with_grads = [output_grads[i] for i in outputs_with_grads_idxs]
@@ -29,13 +32,15 @@ def stage_backward(stage_output, output_grads, input_values, stage_info : str, o
             if isinstance(output_val, torch.Tensor):
                 if not output_val.requires_grad and output_val.grad_fn is None:
                     return
-                assert isinstance(grad_val, (torch.Tensor, type(None))), f'Expected Tensor or None gradient but got {type(grad_val)}'
+                assert isinstance(grad_val, (torch.Tensor, type(None))), \
+                    f'Expected Tensor or None gradient but got {type(grad_val)}'
                 stage_output_tensors.append(output_val)
                 output_grad_tensors.append(grad_val)
             elif isinstance(output_val, (tuple, list)):
                 if grad_val is None:
                     return
-                assert isinstance(grad_val, (tuple, list)), f'grad_value expected to have type {type(output_val)} but got {type(grad_val)}'
+                assert isinstance(grad_val, (tuple, list)), \
+                    f'grad_value expected to have type {type(output_val)} but got {type(grad_val)}'
                 assert len(output_val) == len(grad_val)
                 for ov, gv in zip(output_val, grad_val):
                     extract_tensors_with_grads(ov, gv)
