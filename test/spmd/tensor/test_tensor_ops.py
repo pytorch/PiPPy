5a959f6b0e157c75c42fd3161eb8c72952a8b5bc
Wanchao Liang <wanchaol@fb.com>
2022-08-10 10:59:10 -0700

    address comments

--- test/spmd/tensor/test_tensor_ops.py
+++ test/spmd/tensor/test_tensor_ops.py
@@ -30,18 +30,28 @@ class DistTensorOpsTest(DistTensorTestBase):
     @with_comms
     def test_contiguous(self):
         device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
-        tensor = torch.rand(3, 5, 6)
+        tensor = torch.rand(3, 5, 6, requires_grad=True)
         sharding = [Shard(0)]
         dist_tensor = DTensor.from_local(tensor, device_mesh, sharding)
         self.assertTrue(dist_tensor.is_contiguous())
+        # shard on dim 0 should not change stride (30, 6, 1)
+        self.assertEqual(dist_tensor.stride(), tensor.stride())
 
         new_dt = dist_tensor.transpose(0, 2)
         self.assertFalse(new_dt.is_contiguous())
         self.assertFalse(new_dt.to_local().is_contiguous())
+        # check stride
+        self.assertEqual(new_dt.stride(), (1, 6, 30))
 
         new_dt = new_dt.contiguous()
         self.assertTrue(new_dt.is_contiguous())
         self.assertTrue(new_dt.to_local().is_contiguous())
+        # check stride
+        self.assertEqual(dist_tensor.stride(), tensor.stride())
+
+        # check backward
+        new_dt.to_local().sum().backward()
+        self.assertEqual(tensor.grad, torch.ones(3, 5, 6))
 
     @with_comms
     def test_ones_like(self):
