d616ccddcee3724c36af2f17cc5c11064ee1b794
fduwjj <fduwjj@fb.com>
2022-08-10 17:11:29 +0000

    CI test

--- .github/workflows/spmd_gpu_tests.sh
+++ .github/workflows/spmd_gpu_tests.sh
@@ -23,10 +23,14 @@ pip3 config set global.progress_bar off
 pip3 install flake8 pytest pytest-cov pytest-shard numpy expecttest hypothesis
 if [ -f requirements.txt ]; then pip3 install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html; fi
 
+# Install pavel's huggingface fork
+pip3 install git+https://github.com/huggingface/transformers.git@main sentencepiece
+
 # Install pippy
+python3 setup.py install
 python3 spmd/setup.py install
 
 # Run all integration tests
 # python3 test/spmd/tensor/test_megatron_example.py
 # python3 test/spmd/tensor/test_ddp.py
-python3 test/spmd/tensor/test_tp_sharding_ops.py 
+python3 test/spmd/tensor/test_tp_sharding_ops.py
