448f35c866567dbe10d19580afa16f9d05e978e1
fduwjj <fduwjj@fb.com>
2022-08-23 01:34:44 +0000

    Update docker image and pytorch version

--- .github/workflows/spmd_gpu_tests.sh
+++ .github/workflows/spmd_gpu_tests.sh
@@ -14,14 +14,10 @@ python3 --version
 which pip3
 pip3 --version
 
-# Install git
-apt-get update
-apt-get install git -y
-
 # Install dependencies
 # Turn off progress bar to save logs
 pip3 install --upgrade pip
-if [ -f requirements.txt ]; then pip3 install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu117/torch_nightly.html; fi
+if [ -f requirements.txt ]; then pip3 install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html; fi
 
 # Install pippy
 python3 spmd/setup.py install
@@ -29,4 +25,4 @@ python3 spmd/setup.py install
 set -ex
 
 # Run all integration tests
-pytest --shard-id=${SHARD} --num-shards=4 --cov=spmd test/spmd/
+pytest --shard-id=${SHARD} --num-shards=4 --cov=spmd test/spmd/ 
