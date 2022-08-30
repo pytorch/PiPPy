4327a055899f60ef8a606ddb9555dbdbc9ef9437
Charlie Yan <yanhao.charles@gmail.com>
2022-08-29 21:53:19 -0700

    Fix lint

--- test/spmd/tensor/test_matrix_ops.py
+++ test/spmd/tensor/test_matrix_ops.py
@@ -5,6 +5,7 @@ from spmd.tensor.api import DTensor
 from spmd.test.common_utils import DistTensorTestBase, with_comms  # type: ignore
 from spmd import distribute_tensor, DeviceMesh
 from spmd.tensor.placement_types import Placement, Shard, Replicate, _Partial
+from typing import Sequence, cast
 
 
 class DistMatrixOpsTest(DistTensorTestBase):
@@ -74,32 +75,40 @@ class DistMatrixOpsTest(DistTensorTestBase):
 
         t1 = torch.randn(12, 8, requires_grad=True)
         t2 = torch.randn(8, 16, requires_grad=True)
+        local_res = torch.mm(t1, t2)
 
         def test_placement_comb(
-            placement1: Placement, placement2: Placement
+            placements1: Sequence[Placement], placements2: Sequence[Placement]
         ) -> None:
-            dt1 = distribute_tensor(t1, device_mesh, placement1)
-            dt2 = distribute_tensor(t2, device_mesh, placement2)
-            dist_res: DTensor = torch.mm(dt1, dt2)
-            local_res = torch.mm(t1, t2)
+            dt1 = distribute_tensor(t1, device_mesh, placements1)
+            dt2 = distribute_tensor(t2, device_mesh, placements2)
+            dist_res: DTensor = cast(DTensor, torch.mm(dt1, dt2))
             self.assertEqual(
                 dist_res.redistribute(device_mesh, replica_spec).to_local(),
                 local_res,
             )
             # backward
-            grad_res = torch.ones(12, 16)
-            grad_dist_res = distribute_tensor(
-                grad_res, device_mesh, dist_res.placements
-            )
+            grad_res = torch.ones(dist_res.to_local().shape)
+            grad_dist_res = DTensor(grad_res, device_mesh, dist_res.placements)
             dist_res.backward(grad_dist_res)
             self.assertIsNotNone(dt1.grad)
 
         test_placement_comb(replica_spec, replica_spec)
         test_placement_comb(shard0_spec, replica_spec)
         test_placement_comb(replica_spec, shard1_spec)
+
+        # TODO: support (shard1, shard0) -> [partial]
         with self.assertRaises(Exception):
             test_placement_comb(shard1_spec, shard0_spec)
+
+        # TODO: support (shard0, shard1) -> [shard0, shard1]
+        with self.assertRaises(Exception):
+            test_placement_comb(shard0_spec, shard1_spec)
+
+        with self.assertRaises(Exception):
             test_placement_comb(replica_spec, shard0_spec)
+
+        with self.assertRaises(Exception):
             test_placement_comb(shard1_spec, replica_spec)
 
     @with_comms
