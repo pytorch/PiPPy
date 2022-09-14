8bccfaffc4980bf0ff6b9580dbba981d772dafdb
Wanchao Liang <wanchaol@fb.com>
2022-09-14 10:54:57 -0700

    fix lint

--- test/spmd/tensor/test_common_rules.py
+++ test/spmd/tensor/test_common_rules.py
@@ -3,7 +3,11 @@ import torch
 from torch.testing._internal.common_utils import run_tests
 from spmd.tensor.dispatch import OpSchema
 
-from spmd.tensor.ops.common_rules import einop_rule, reduction_rule, pointwise_rule
+from spmd.tensor.ops.common_rules import (
+    einop_rule,
+    reduction_rule,
+    pointwise_rule,
+)
 from spmd.tensor.placement_types import DTensorSpec
 from spmd.testing.common_utils import (  # type: ignore
     DistTensorTestBase,
