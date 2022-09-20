46b999aa32a74839d273c34b105c9ea310a6fefd
Alisson Azzolini <37222419+aazzolini@users.noreply.github.com>
2022-09-19 17:58:54 -0700

    mark tranpose is a linear op

--- test/spmd/tensor/test_matrix_ops.py
+++ test/spmd/tensor/test_matrix_ops.py
@@ -3,6 +3,7 @@ import torch
 from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from spmd.tensor.api import DTensor
+from spmd.tensor.redistribute import redistribute_spmd_tensor
 from spmd.testing.common_utils import (  # type: ignore
     DistTensorTestBase,
     with_comms,
@@ -121,6 +122,29 @@ class DistMatrixOpsTest(DistTensorTestBase):
         tranposed_mat2 = tranposed_mat.t()
         self.assertEqual(tranposed_mat2.size(), torch.Size([12, 8]))
         self.assertEqual(tranposed_mat2.placements, shard_spec)
+
+    @with_comms
+    def test_t_partial(self):
+        device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
+
+        a = torch.randn(12, 8)
+        b = torch.randn(8, 4)
+        c = torch.mm(a, b).t()
+
+        da = distribute_tensor(a, device_mesh, [Shard(1)])
+        db = distribute_tensor(b, device_mesh, [Shard(0)])
+
+        # mm(da, db) should return a _Partial tensor.
+        # transposing it should keep it _Partial
+        dc = torch.mm(da, db).t()
+
+        self.assertTrue(isinstance(dc.placements[0], _Partial))
+
+        # check that the local and distributed op results match
+        self.assertEqual(
+            c,
+            redistribute_spmd_tensor(dc, device_mesh, [Replicate()]).to_local(),
+        )
 
     # baddbmm introduces nan occasionally on CPU: https://github.com/pytorch/pytorch/issues/80588
     @with_comms
