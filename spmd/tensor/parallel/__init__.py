5cc8e35be3eeebd2cc51c498f4e309a3185468aa
Crutcher Dunnavant <crutcher@gmail.com>
2022-09-27 18:58:58 -0700

    fix

--- spmd/tensor/parallel/__init__.py
+++ spmd/tensor/parallel/__init__.py
@@ -1,1 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
+from spmd.tensor.parallel.multihead_attention_tp import (
+    TensorParallelMultiheadAttention,
+)
+
+__all__ = [
+    "TensorParallelMultiheadAttention",
+]
