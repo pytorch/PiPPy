5089df87620461ceb58d7bf82514db4bd06e89a8
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2022-10-11 14:05:10 -0700

    [spmd] Self attention module sharding api (#523)
    * added module sharding APIs for Self Attention module to package spmd.tensor.parallel
    
    * added test for TensorParallelMultiheadAttention == distributed_module(TensorParallelMultiheadAttention)
    
    * added test for TensorParallelMultiheadAttention == distributed_module(torch.nn.MultiheadAttention)
    
    * TODO: 
    - see why TensorParallelMultiheadAttention != torch.nn.MultiheadAttention
    - add sharding API for MLP

--- spmd/tensor/parallel/__init__.py
+++ spmd/tensor/parallel/__init__.py
@@ -2,3 +2,9 @@
 from spmd.tensor.parallel.multihead_attention_tp import (
     TensorParallelMultiheadAttention,
 )
+
+from spmd.tensor.parallel.api import (
+    tp_shard_self_attn,
+    replicate_input,
+    replicate_output,
+)
