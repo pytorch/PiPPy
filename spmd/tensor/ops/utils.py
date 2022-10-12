b4b5bab8dc419d09041d973d27b833d5669b0f00
Shen Li <cs.shenli@gmail.com>
2022-10-12 22:19:45 +0000

    Update base for Update on "Introducing SPMD API"
    [ghstack-poisoned]

--- spmd/tensor/ops/utils.py
+++ spmd/tensor/ops/utils.py
@@ -43,7 +43,7 @@ def register_prop_rule(func):
 
 def as_list(
     x: Union[List[object], object]
-# pyre-fixme[11]: Annotation `immutable_list` is not defined as a type.
+    # pyre-fixme[11]: Annotation `immutable_list` is not defined as a type.
 ) -> Union[List[object], torch.fx.immutable_collections.immutable_list]:
     # During tracing, `aten.sum.dim_IntList` uses `immutable_list` for its args,
     # which is an object but treated as a list by the tracer. Therefore, keep
