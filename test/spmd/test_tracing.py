49a055c9cad72b9cccbe7619084ac3a34feef811
Alisson Azzolini <37222419+aazzolini@users.noreply.github.com>
2022-11-23 10:23:56 -0800

    Fix tracer for dict and list inputs (#647)
    We need to flatten input arguments first so that we can add all tensor
    inputs to the list.

--- test/spmd/test_tracing.py
+++ test/spmd/test_tracing.py
@@ -194,7 +194,7 @@ class TraceModuleTest(DTensorTestBase):
     def world_size(self):
         return 2
 
-    def _test_trace_replicate(self, model, x):
+    def _test_trace_replicate(self, model, x, *args, **kwargs):
         # if x.device.type == "cuda":
         ddp = DDP(deepcopy(model))
         spmd = SPMD(
@@ -207,8 +207,8 @@ class TraceModuleTest(DTensorTestBase):
             ),
         )
 
-        ddp(x).sum().backward()
-        spmd(x).sum().backward()
+        ddp(x, *args, **kwargs).sum().backward()
+        spmd(x, *args, **kwargs).sum().backward()
         for p1, p2 in zip(ddp.parameters(), spmd.parameters()):
             # DDP divides gradients by world size to compute average, but
             # _Partial tensor shouldn't do that automatically. Hence explicitly
@@ -224,14 +224,20 @@ class TraceModuleTest(DTensorTestBase):
                 self.w = torch.nn.Parameter(torch.rand((5, 10)))
                 self.b = torch.nn.Parameter(torch.rand((5)))
 
-            def forward(self, x):
+            def forward(self, x, *args, **kwargs):
+                # the code below will bake in the shape of x_t as arguments to expand
                 x_t = x.permute(0, 2, 1)
+                y_t = kwargs["dict_test"]["value"].expand(x_t.shape) + args[0][
+                    0
+                ].expand(x_t.shape)
                 # code below triggers an "expand" with shape baked in.
-                return torch.nn.functional.linear(x_t, self.w, self.b)
+                return torch.nn.functional.linear(y_t, self.w, self.b)
 
         model = LCE().to(self.device_type)
         x = torch.randn(2, 10, 80).to(self.device_type)
-        self._test_trace_replicate(model, x)
+        y = torch.randn(2, 80, 10).to(self.device_type)
+        z = torch.randn(2, 80, 10).to(self.device_type)
+        self._test_trace_replicate(model, x, [y], dict_test={"value": z})
 
     @with_comms
     def test_sequential(self):
