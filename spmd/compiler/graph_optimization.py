1b756d637d2d1dffcc5eb8e5471059b232d40ab2
lessw2020 <lessw@etrillium.com>
2022-12-10 00:15:49 +0000

    expose fusion_policy in graph_opt fuse_communication

--- spmd/compiler/graph_optimization.py
+++ spmd/compiler/graph_optimization.py
@@ -165,13 +165,14 @@ class DistGraphOptimization:
         self,
         bucketing_strategy: BucketingStrategy = BucketingStrategy.FIXED,
         scheduling_policy: SchedulingPolicy = SchedulingPolicy.FCFS,
+        fusion_policy: int = 10,
     ) -> "DistGraphOptimization":
 
         assert len(
             self._graph.bwd_graph_modules
         ), f"no bwd graph ready from {self._graph.bwd_graph_modules}"
 
-        run_fuse_communication(self._graph.bwd_graph_modules[0])
+        run_fuse_communication(self._graph.bwd_graph_modules[0], fusion_policy)
         return self
 
     @graph_optimization_pass()
