3b612c9f28b6fe451c6954f82b80066ac0738b4f
Anjali Sridhar <anj@fb.com>
2023-01-26 22:33:02 +0000

    more cleanup

--- spmd/compiler/distribute.py
+++ spmd/compiler/distribute.py
@@ -119,7 +119,7 @@ def _get_dtensor_dispatch_graph(
     node: fx.Node,
     node_to_obj: Dict[fx.Node, object],
 ) -> fx.GraphModule:
-    def remap_arg(arg: object) -> object:
+    def _remap_arg(arg: object) -> object:
         if isinstance(arg, torch.fx.Node):
             obj = node_to_obj[arg]
             if _get_tracer():
@@ -131,7 +131,7 @@ def _get_dtensor_dispatch_graph(
             return arg
 
     # Args should be a list of objects post remapping.
-    args = tree_map(remap_arg, node.args)
+    args = tree_map(_remap_arg, node.args)
     # kwargs in this set of tests are all constants
     kwargs = cast(Dict[str, object], node.kwargs)
 
@@ -181,7 +181,7 @@ def _build_dummy_add_graph(
     dt: DTensor, node_to_obj: Dict[fx.Node, object]
 ) -> Tuple[fx.GraphModule, object]:
     """
-    creates a graph for a dummy add function from a partial DTensor.
+    Creates a graph for a dummy add function from a partial DTensor.
     This dummy add is used for triggering all_reduce on a Partial DTensor
     during the DTensor expansion of the traced graph.
     Also returns the actual DTensor after resharding.
@@ -210,6 +210,8 @@ def _build_dummy_add_graph(
 
     traced_dispatch.graph.lint()
 
+    # TODO(anj): This depends on the call function node -> actual DTensor output
+    # mapping that we want to avoid for SPMD expansion
     return traced_dispatch, node_to_obj[call_functions[0]]
 
 
@@ -250,7 +252,10 @@ def _convert_output(
         traced_dispatch.graph.lint()
         traced_dispatch.graph.eliminate_dead_code()
         # also update the actual DTensor corresponding to the node
+        # TODO(anj): We require mapping of the final DTensor output to the wait
+        # comm node. 
         node_to_obj[wait[0]] = result_obj
+        # print(f"wait[0] {wait[0]} result_obj {result_obj}")
 
         value_remap: Dict[fx.Node, fx.Node] = {}
         for dtn in traced_dispatch.graph.nodes:
@@ -258,6 +263,10 @@ def _convert_output(
                 # do nothing, ignore placeholders, as it has
                 # already been prepared in value_remap
                 value_remap[dtn] = argument
+                # if torch.distributed.get_rank() == 0:
+                #     print(f"value_remap[dtn] {value_remap[dtn]}")
+                #     print(f"dtn.args {dtn}")
+                #     print(f"new_args {new_args}")
             elif dtn.op == OP.OUTPUT:
                 assert (
                     len(dtn.args) == 1 and len(dtn.args[0]) == 1
@@ -266,9 +275,15 @@ def _convert_output(
                 # the concrete DTensor value of output was added when creating the
                 # inner graph (in _build_dummy_add_graph). Just add it to the final
                 # output node so that we can report the final output specs correctly.
+                # TODO(anj): We are depending on the concrete DTensor output of the dummy add.
                 node_to_obj[value_remap[dtn.args[0][0]]] = node_to_obj[
                     dtn.args[0][0]
                 ]
+                # if torch.distributed.get_rank() == 0:
+                #     print(f"value_remap[dtn.args[0][0]] {value_remap[dtn.args[0][0]]}")
+                #     print(f"dtn.args[0][0] {dtn.args[0][0]}")
+                #     print(f"new_args {new_args}")
+
             else:
                 if dtn.op == OP.GET_ATTR:
                     setattr(
@@ -293,8 +308,15 @@ def _rebuild_graph(
     gm: fx.GraphModule,
     node_replacements: Dict[torch.fx.Node, torch.fx.GraphModule],
 ) -> None:
+
+    if torch.distributed.get_rank() == 0:
+        for k, v in node_replacements.items():
+            print(f"node_replacements node.name: {k.name} replacement graph {v.graph}")
+    
+        print(f"orig graph {gm.graph}")
+
     # replace nodes in local traced graph with DTensor's dispatch graph
-    new_nodes: Dict[torch.fx.Node, torch.fx.Node] = {}
+    # new_nodes: Dict[torch.fx.Node, torch.fx.Node] = {}
     for node in gm.graph.nodes:
         if node not in node_replacements:
             continue
@@ -309,16 +331,16 @@ def _rebuild_graph(
             if dtn.op == OP.PLACEHOLDER:
                 value_remap[dtn] = flatten_args[i]
                 i += 1
-
+        print(f"node {node.name} value_remap {value_remap}")
         # insert DT's dispatch graph to traced local graph.
         with gm.graph.inserting_before(node):
             for dtn in traced_dispatch.graph.nodes:
+                
                 if dtn.op == OP.PLACEHOLDER:
                     # do nothing, ignore placeholders, as it has already
                     # been prepared in value_remap
                     pass
                 elif dtn.op == OP.OUTPUT:
-                    # TODO: AssertionError: Expecting single output, but got ([getitem, getitem_1, getitem_2],)
                     assert (
                         len(dtn.args) == 1
                     ), f"Expecting single output, but got {dtn.args} {len(dtn.args[0])}"
@@ -349,37 +371,40 @@ def _rebuild_graph(
 
                     new_node = value_remap[output]
                     node.replace_all_uses_with(new_node)
-                    new_nodes[node] = new_node
+                    # new_nodes[node] = new_node
+                    print(f"rebuild graph {output}")
+                    print(f"node {node.name} new_node {new_node.name} dtn {dtn.name} dtn.op {dtn.op} value_remap {value_remap}")
                 else:
                     value_remap[dtn] = gm.graph.node_copy(
                         dtn, lambda n: value_remap[n]
                     )
+                    print(f"node {node.name} dtn {dtn.name} dtn.op {dtn.op} value_remap {value_remap}")
 
     gm.graph.lint()
     gm.graph.eliminate_dead_code()
     gm.recompile()
 
 
-def get_user_to_last_uses(
+def _get_last_consumer_to_nodes(
     graph: fx.Graph,
 ) -> Dict[fx.Node, List[fx.Node]]:
     # Run through reverse nodes and record the first instance of a use
     # of a given node. This represents the *last* use of the node in the
     # execution order of the program, which we will use to free unused
     # values
-    node_to_last_use: Dict[fx.Node, fx.Node] = {}
-    user_to_last_uses: Dict[fx.Node, List[fx.Node]] = {}
+    node_to_last_consumer: Dict[fx.Node, fx.Node] = {}
+    last_consumer_to_nodes: Dict[fx.Node, List[fx.Node]] = {}
 
-    def register_last_uses(n: fx.Node, user: fx.Node) -> None:
-        if n not in node_to_last_use:
-            node_to_last_use[n] = user
-            user_to_last_uses.setdefault(user, []).append(n)
+    def _register_final_consumer(arg_node: fx.Node, consumer: fx.Node) -> None:
+        if arg_node not in node_to_last_consumer:
+            node_to_last_consumer[arg_node] = consumer
+            last_consumer_to_nodes.setdefault(consumer, []).append(arg_node)
 
     for node in reversed(graph.nodes):
-        fx.node.map_arg(node.args, lambda n: register_last_uses(n, node))
-        fx.node.map_arg(node.kwargs, lambda n: register_last_uses(n, node))
+        fx.node.map_arg(node.args, lambda arg_node: _register_final_consumer(arg_node, node))
+        fx.node.map_arg(node.kwargs, lambda kwarg_node: _register_final_consumer(kwarg_node, node))
 
-    return user_to_last_uses
+    return last_consumer_to_nodes
 
 
 def _convert_to_distributed(
@@ -398,7 +423,7 @@ def _convert_to_distributed(
     # DTensor ops.
     node_replacements: Dict[torch.fx.Node, torch.fx.GraphModule] = {}
 
-    user_to_last_uses = get_user_to_last_uses(gm.graph)
+    last_consumer_to_nodes = _get_last_consumer_to_nodes(gm.graph)
 
     output_schemas: Dict[str, Schema] = {}
     for i, node in enumerate(gm.graph.nodes):
@@ -425,26 +450,29 @@ def _convert_to_distributed(
             #     print(f"g {node_replacements[node].graph}")
         elif node.op == OP.OUTPUT:
             if not _allow_partial:
-                # returns the possibly modified output node
+                # Returns an expanded dummy add node that ensures
+                # that the partial output tensor has been converted
+                # to a replicated tensor.
                 node = _convert_output(gm, node, node_to_obj)
+                if torch.distributed.get_rank() == 0:
+                    print(f'OUTPUT node {node.target}')
 
-            # save output sharding for the inputs to backward pass
-            for a in node.args[0]:
-                if isinstance(a, fx.Node):
-                    obj = node_to_obj[a]
+            # Save output sharding for the inputs to backward pass.
+            # TODO(anj): Pipe the output schema for the BW pass
+            # instead of requiring the full output DTensor to be
+            # materialized.
+            for inp_arg in node.args[0]:
+                if isinstance(inp_arg, fx.Node):
+                    obj = node_to_obj[inp_arg]
                     if isinstance(obj, DTensor):
-                        output_schemas[a.name] = Schema(
+                        output_schemas[inp_arg.name] = Schema(
                             obj.device_mesh, obj.placements  # type: ignore
                         )
 
         elif node.op == OP.CALL_FUNCTION:
-
-            def remap_arg(arg: object) -> object:
+            def _remap_arg(arg: object) -> object:
                 if isinstance(arg, torch.fx.Node):
                     obj = node_to_obj[arg]
-                    # TODO(anj): we need this for getitem but can we be more generic?
-                    if isinstance(obj, tuple):
-                        return obj
                     if _get_tracer():
                         # This is a shared arg, already has a tracer from previous
                         # tracing. Delete the tracer.
@@ -453,15 +481,20 @@ def _convert_to_distributed(
                 else:
                     return arg
 
-            args = tree_map(remap_arg, node.args)
+            args = tree_map(_remap_arg, node.args)
+            assert len(args) >= 2, f"Expected number of args for call function to be at least 2, found {len(args)}"
+            # TODO(anj): Why do we assume this is only 2?
             node_to_obj[node] = node.target(args[0], args[1])
+            if torch.distributed.get_rank() == 0:
+                print(f"args[0], args[1] {args[0], args[1]}")
+                print(f"node.target(args[0], args[1]) {node.target(args[0], args[1])}")
         else:
-            raise ValueError(f"Unrecognized node {node}")
+            raise ValueError(f"Unrecognized node.op type {node.op}")
 
-        if node in user_to_last_uses:
-            # save memory by deleting objs that wont be used anymore
-            for last_use in user_to_last_uses[node]:
-                del node_to_obj[last_use]
+        if node in last_consumer_to_nodes:
+            # Save memory by deleting objs that wont be used anymore.
+            for arg_node in last_consumer_to_nodes[node]:
+                del node_to_obj[arg_node]
 
     _rebuild_graph(gm, node_replacements)
 
