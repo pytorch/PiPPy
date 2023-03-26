f282596f188e7530210c48defe5c4d12b73885e9
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-03-26 05:54:50 +0000

    added scripts for bulding and uploading binaries to pypi

---
+++ binaries/build.py
@@ -0,0 +1,84 @@
+import argparse
+import glob
+import os
+import sys
+
+# To help discover local modules
+REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+sys.path.append(REPO_ROOT)
+
+
+def build_dist_whl(args):
+    """
+    Function to build the wheel files for torchserve, model-archiver and workflow-archiver
+    """
+    if args.nightly:
+        print(
+            "## Started pippy build"
+        )
+        create_wheel_cmd = "python setup.py "
+    else:
+        print("## Started pippy build")
+        create_wheel_cmd = "python setup.py bdist_wheel --release"
+
+    cur_dir = REPO_ROOT
+        
+    os.chdir(cur_dir)
+
+    cur_wheel_cmd = (
+        create_wheel_cmd + "--override-name " + binary + "-nightly" + " bdist_wheel"
+        if args.nightly
+        else create_wheel_cmd
+    )
+
+    # Build wheel
+    print(f"## In directory: {os.getcwd()} | Executing command: {cur_wheel_cmd}")
+
+    if not args.dry_run:
+        build_exit_code = os.system(cur_wheel_cmd)
+        # If any one of the steps fail, exit with error
+        if build_exit_code != 0:
+            sys.exit(f"## {binary} build Failed !")
+
+
+def build(args):
+
+    # Build dist wheel files
+    build_dist_whl(args)
+
+    os.chdir(REPO_ROOT)
+
+    if not args.dry_run:
+        ts_wheel_path = glob.glob(os.path.join(REPO_ROOT, "dist", "*.whl"))[0]
+
+    else:
+        ts_wheel_path = os.path.join(REPO_ROOT, "dist", "*.whl")
+
+    print(f"## PiPPY wheel location: {ts_wheel_path}")
+
+
+    # If conda build fails, exit with error
+    if conda_build_exit_code != 0:
+        sys.exit("## Conda Build Failed !")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Build for pippy"
+    )
+    parser.add_argument(
+        "--nightly",
+        action="store_true",
+        required=False,
+        help="specify nightly is being built",
+    )
+
+    parser.add_argument(
+        "--dry_run",
+        action="store_true",
+        help="dry_run will print the commands that will be run without running them",
+    )
+
+    args = parser.parse_args()
+
+    build(args)
---
+++ binaries/upload.py
@@ -0,0 +1,79 @@
+#! /usr/env/bin
+import argparse
+import glob
+import os
+import sys
+
+# To help discover local modules
+REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+sys.path.append(REPO_ROOT)
+
+def try_and_handle(cmd, dry_run=False):
+    if dry_run:
+        print(f"Executing command: {cmd}")
+    else:
+        try:
+            subprocess.run([cmd], shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            raise (e)
+
+def upload_pypi_packages(args, WHL_PATHS):
+    """
+    Takes a list of path values and uploads them to pypi using twine, using token stored in environment variable
+    """
+    dry_run = args.dry_run
+
+    # Note: TWINE_USERNAME and TWINE_PASSWORD are expected to be set in the environment
+    for dist_path in WHL_PATHS:
+        if args.test_pypi:
+            try_and_handle(
+                f"twine upload {dist_path}/* --username __token__ --repository-url https://test.pypi.org/legacy/",
+                dry_run,
+            )
+        else:
+            try_and_handle(f"twine upload --username __token__ {dist_path}/*", dry_run)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Upload anaconda and pypi packages for torchserve and torch-model-archiver"
+    )
+    parser.add_argument(
+        "--upload-conda-packages",
+        action="store_true",
+        required=False,
+        help="Specify whether to upload conda packages",
+    )
+    parser.add_argument(
+        "--upload-pypi-packages",
+        action="store_true",
+        required=False,
+        help="Specify whether to upload pypi packages",
+    )
+    parser.add_argument(
+        "--test-pypi",
+        action="store_true",
+        required=False,
+        help="Specify whether to upload to test PyPI",
+    )
+
+    parser.add_argument(
+        "--dry_run",
+        action="store_true",
+        help="dry_run will print the commands that will be run without running them. Only works for pypi now",
+    )
+    args = parser.parse_args()
+
+    PACKAGES = ["torchserve", "model-archiver", "workflow-archiver"]
+    CONDA_PACKAGES_PATH = os.path.join(REPO_ROOT, "binaries", "conda", "output")
+
+    if not args.dry_run:
+        PiPPY_WHEEL_PATH = glob.glob(os.path.join(REPO_ROOT, "dist"))[0]
+    else:
+        PiPPY_WHEEL_PATH = os.path.join(REPO_ROOT, "dist")
+      
+
+    WHL_PATHS = [PiPPY_WHEEL_PATH]
+
+    if args.upload_pypi_packages:
+        upload_pypi_packages(args, WHL_PATHS)
