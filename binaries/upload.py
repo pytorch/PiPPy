9e2abb58e99bf9255fa6f614a588770b2e142ddf
Ke Wen <kw2501@fb.com>
2023-04-11 00:56:49 +0000

    Add back --test-pypi, add --upload, remove --dry-run

--- binaries/upload.py
+++ binaries/upload.py
@@ -4,11 +4,13 @@ import glob
 import os
 import sys
 
+
 # To help discover local modules
 REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 sys.path.append(REPO_ROOT)
 
-def try_and_handle(cmd, dry_run=False):
+
+def exe_cmd(cmd, dry_run=True):
     if dry_run:
         print(f"Executing command: {cmd}")
     else:
@@ -17,21 +19,25 @@ def try_and_handle(cmd, dry_run=False):
         except subprocess.CalledProcessError as e:
             raise (e)
 
+
 def upload_pypi_packages(args, WHL_PATHS):
     """
     Takes a list of path values and uploads them to pypi using twine, using token stored in environment variable
     """
-    dry_run = args.dry_run
+    dry_run = not args.upload
 
     # Note: TWINE_USERNAME and TWINE_PASSWORD are expected to be set in the environment
+    options = "--username __token__ "
+
+    if args.test_pypi:
+        options += "--repository-url https://test.pypi.org/legacy/ "
+        # TODO:
+        # maybe "--repository testpypi " works the same (and shorter)?
+        # Ref: https://packaging.python.org/en/latest/tutorials/packaging-projects/#uploading-the-distribution-archives
+
     for dist_path in WHL_PATHS:
-        if args.test_pypi:
-            try_and_handle(
-                f"twine upload {dist_path}/* --username __token__ --repository-url https://test.pypi.org/legacy/",
-                dry_run,
-            )
-        else:
-            try_and_handle(f"twine upload --username __token__ {dist_path}/*", dry_run)
+        cmd = "twine upload " + options + f" {dist_path}/*"
+        exe_cmd(cmd, dry_run)
 
 
 if __name__ == "__main__":
@@ -40,29 +46,27 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--upload-pypi-packages",
+        "--upload",
         action="store_true",
         required=False,
-        help="Specify whether to upload pypi packages",
+        help="Actually upload packages; otherwise dry run",
     )
 
     parser.add_argument(
-        "--dry_run",
+        "--test-pypi",
         action="store_true",
-        help="dry_run will print the commands that will be run without running them. Only works for pypi now",
+        help="Upload to test.pypi instead of pypi",
     )
+
     args = parser.parse_args()
 
     PACKAGES = ["pippy"]
-    
 
-    if not args.dry_run:
+    if args.upload:
         PiPPY_WHEEL_PATH = glob.glob(os.path.join(REPO_ROOT, "dist"))[0]
     else:
         PiPPY_WHEEL_PATH = os.path.join(REPO_ROOT, "dist")
       
-
     WHL_PATHS = [PiPPY_WHEEL_PATH]
 
-    if args.upload_pypi_packages:
-        upload_pypi_packages(args, WHL_PATHS)
+    upload_pypi_packages(args, WHL_PATHS)
