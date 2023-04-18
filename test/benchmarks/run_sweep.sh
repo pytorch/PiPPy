e5c089dc3095a30585fea3da08635cc725aabf24
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-04-18 03:50:16 +0000

    update

--- test/benchmarks/run_sweep.sh
+++ test/benchmarks/run_sweep.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-python pippy_decoders_benchmarks.py  --model_name 'facebook/opt-30b' --batch-size 1 --chunks 1
+python pippy_decoders_benchmarks.py  --model_name 'facebook/opt-6.7b' --batch-size 1 --chunks 1 --log_finename 'pippy_half.csv'
 
 
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 2 --chunks 1
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 2 --chunks 2
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 2 --chunks 1
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 2 --chunks 2
 
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 4 --chunks 1
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 4 --chunks 2
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 4 --chunks 4
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 4 --chunks 1
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 4 --chunks 2
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 4 --chunks 4
 
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 1
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 2
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 4
-
-
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 1
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 2
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 4
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 1
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 2
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 4
 
 
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 16 --chunks 1
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 16 --chunks 2
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 16 --chunks 4
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 1
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 2
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 8 --chunks 4
 
 
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 32 --chunks 1
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 32 --chunks 2
-python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 32 --chunks 4
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 16 --chunks 1
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 16 --chunks 2
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 16 --chunks 4
+
+
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 32 --chunks 1
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 32 --chunks 2
+# python pippy_decoders_benchmarks.py --model_name 'facebook/opt-30b' --batch-size 32 --chunks 4
 
 
 python pippy_decoders_benchmarks.py --batch-size 64 --chunks 1
