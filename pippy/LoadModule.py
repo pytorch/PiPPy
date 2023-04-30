86f3f8b6c6334ffaad94768aa264a0c6c9925ab6
Ke Wen <kw2501@fb.com>
2023-04-30 20:51:05 +0000

    Merge path of shared weight loading

--- pippy/LoadModule.py
+++ pippy/LoadModule.py
@@ -29,7 +29,7 @@ def load_checkpoint(
 
     prefix_to_test = [checkpoint_prefix] if checkpoint_prefix else TYPICAL_PREFIXES
 
-    file_to_weights = get_file_to_weight_map(model, index, prefix_to_test)
+    file_to_weights = _get_file_to_weight_map(model, index, prefix_to_test)
 
     used_files = file_to_weights.keys()
     import time
@@ -43,14 +43,12 @@ def load_checkpoint(
 
         if file in file_to_weights:
             weights = file_to_weights[file]
-            for new_name, old_name in weights:
+            for new_name, old_name, clone in weights:
                 assert old_name in checkpoint.keys(), f"{old_name} not in {file}"
                 loaded_weight = checkpoint[old_name]
-                set_module_tensor_to_device(
-                    model, new_name, device, value=loaded_weight, dtype=dtype
+                _set_module_tensor_to_device(
+                    model, new_name, device, value=loaded_weight, dtype=dtype, clone=clone,
                 )
-
-        handle_shared_weights(model, checkpoint, device=device, dtype=dtype)
 
         del checkpoint
         gc.collect()
@@ -58,7 +56,7 @@ def load_checkpoint(
     return model
 
 
-def get_file_to_weight_map(
+def _get_file_to_weight_map(
     model: nn.Module,
     index,
     prefix_to_test: List[str],
@@ -71,8 +69,9 @@ def get_file_to_weight_map(
     ]:
         for new_name, _ in iterator:
             old_name = model.remap_qualname(new_name)
-            cp_weight_name = match_checkpoint_name(old_name, index, prefix_to_test)
+            cp_weight_name, clone_needed = _match_checkpoint_name(old_name, index, prefix_to_test)
             if cp_weight_name is None:
+                # Not found
                 logging.warning(
                     f"Weight {new_name} maps to {old_name}, "
                     f"but {old_name} is not found in checkpoint index"
@@ -80,34 +79,73 @@ def get_file_to_weight_map(
                 continue
             file = index[cp_weight_name]
             weights = file_to_weights.setdefault(file, [])
-            weights.append((new_name, cp_weight_name))
+            weights.append((new_name, cp_weight_name, clone_needed))
 
     return file_to_weights
 
 
-def match_checkpoint_name(
+# Some weights like word_embeddings.weight and shared.weight will be used in
+# different layers, but these layers may not in the index file, so we can only
+# clone the shared weight to their corresponding layers.
+
+TYPICAL_SHARER_WEIGHTS = [
+    "lm_head.weight",  # facebook/opt-6.7b
+    "encoder_embed_tokens.weight",
+]
+
+TYPICAL_SHAREE_WEIGHTS = [
+    "decoder.embed_tokens.weight",
+    "word_embeddings.weight",
+    "shared.weight",
+    "wte.weight",
+]
+
+
+def _match_checkpoint_name(
     old_name: str,
     index,
     prefix_to_test: List[str],
-):
+) -> Tuple[str, bool]:
+    """
+    A helper function to match weight name against those in checkpoint index.
+    Args:
+        old_name (`str`):
+            weight name in original model (retrieved via `remap_qualname()`)
+        index (`Dict`?):
+            checkpoint index
+        prefix_to_test (`List[str]`):
+            prefix to try if direct match is not found
+    Return:
+        weight name in the checkpoint index and whether clone is needed
+    Search rule:
+        - Exact match, no need to clone
+        - Match after prefix, no need to clone
+        - Match via shared weight table, clone needed
+    """
     if old_name in index.keys():
-        return old_name
+        return old_name, False
 
     for prefix in prefix_to_test:
         if (old_name.startswith(prefix)
             and old_name[len(prefix)+1 :] in index.keys()
         ):
-            return old_name[len(prefix)+1 :]
+            return old_name[len(prefix)+1 :], False
 
-    return None
+    if old_name in TYPICAL_SHARER_WEIGHTS:
+        for sharee in TYPICAL_SHAREE_WEIGHTS:
+            if sharee in index.keys():
+                return sharee, True
+
+    return None, False
 
 
-def set_module_tensor_to_device(
+def _set_module_tensor_to_device(
     module: nn.Module,
     qualname: str,
     device: Optional[torch.device] = None,
     value: Optional[torch.Tensor] = None,
     dtype: Optional[Union[str, torch.dtype]] = None,
+    clone: bool = False,
 ):
     """
     A helper function to set a given tensor (parameter of buffer) of a module on a specific device (note that doing
@@ -124,6 +162,8 @@ def set_module_tensor_to_device(
         dtype (`torch.dtype`, *optional*):
             If passed along the value of the parameter will be cast to this `dtype`. Otherwise, `value` will be cast to
             the dtype of the existing parameter in the model.
+        clone (`bool`, default is False):
+            whether to copy the input value.
     """
     # Recurse if needed
     if value is None:
@@ -162,7 +202,13 @@ def set_module_tensor_to_device(
     with torch.no_grad():
         if isinstance(value, torch.Tensor):
             new_value = value.to(device=device, dtype=dtype)
+            # In case device and dtype do not change, `new_value` would point to
+            # the same storage as `value`, and we would need to explicitly clone
+            if clone and new_value.data_ptr() == value.data_ptr():
+                new_value = value.clone()
         else:
+            # Note: `torch.tensor()` allocates new memory to copy the data of
+            # tensor, so clone is taken care of
             new_value = torch.tensor(value, device=device, dtype=dtype)
 
         if is_buffer:
@@ -179,40 +225,3 @@ def set_module_tensor_to_device(
                     new_value, requires_grad=old_value.requires_grad
                 )
             submod._parameters[weight] = new_param
-
-
-# Some weights like word_embeddings.weight and shared.weight will be used in
-# different layers, but these layers may not in the index file, so we can only
-# clone the shared weight to their corresponding layers.
-def handle_shared_weights(
-    module: nn.Module,
-    checkpoint,
-    device: torch.device = None,
-    dtype: torch.dtype = None,
-):
-    for param_name, param in checkpoint.items():
-        if param_name in [
-            "word_embeddings.weight",
-            "shared.weight",
-            "wte.weight",
-        ]:
-            if hasattr(module, "lm_head"):
-                module.lm_head.weight = torch.nn.Parameter(  # type: ignore[union-attr]
-                    (param.clone()).to(device).to(dtype)
-                )
-            if hasattr(module, "encoder_embed_tokens"):
-                module.encoder_embed_tokens.weight = torch.nn.Parameter(  # type: ignore[union-attr]
-                    (param.clone()).to(device).to(dtype)
-                )
-            if hasattr(module, "decoder_embed_tokens"):
-                module.decoder_embed_tokens.weight = torch.nn.Parameter(  # type: ignore[union-attr]
-                    (param.clone()).to(device).to(dtype)
-                )
-        elif param_name in [
-            "decoder.embed_tokens.weight",
-        ]:
-            # For OPT, the lm_head weight is automatically tied to the embed tokens weight
-            if hasattr(module, "lm_head"):
-                module.lm_head.weight = torch.nn.Parameter(  # type: ignore[union-attr]
-                    (param.clone()).to(device).to(dtype)
-                )
