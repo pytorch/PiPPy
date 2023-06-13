8f549f37f96e8c89e4c6dd3e1caf358273ce8032
eddogola <64967909+eddogola@users.noreply.github.com>
2023-06-12 17:47:09 -0700

    Add docstring for load_checkpoint (#807)
    ## Description
    
    Added docstrings to load_checkpoint module and _get_file_to_weight_map
    function.
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ---------
    
    Co-authored-by: Eddy <ogolaedd@meta.com>

--- pippy/LoadModule.py
+++ pippy/LoadModule.py
@@ -20,7 +20,22 @@ def load_checkpoint(
     device: torch.device = None,
     dtype: torch.dtype = None,
     checkpoint_prefix: str = None,
-):
+) -> nn.Module:
+    """
+    Load a checkpoint from a model file.
+    Args:
+        model (`torch.nn.Module`): the model to load the checkpoint into
+        index_filename (`Union[str, os.PathLike]`): path to the checkpoint's index (metadata file)
+        device (`torch.device`): the device on which to load the checkpoint
+        dtype (`torch.dtype`): the dtype on which to load the checkpoint
+        checkpoint_prefix (`str`): the prefix of the checkpoint to load
+    Returns:
+        The loaded checkpoint model
+    Example:
+        ```
+        checkpoint = load_checkpoint(model, index_filename, device, dtype)
+        ```
+    """
     checkpoint_folder = os.path.split(index_filename)[0]
     with open(index_filename, "r") as f:
         index = json.loads(f.read())
@@ -68,9 +83,20 @@ def load_checkpoint(
 
 def _get_file_to_weight_map(
     model: nn.Module,
-    index,
+    index: Dict[str, str],
     prefix_to_test: List[str],
 ) -> Dict[str, List[Tuple]]:
+    """
+    A helper function to create a mapping from binary checkpoint filename to parameter names
+    Args:
+        model (`torch.nn.Module`): The model to load weights into
+        index (`Dict[str, str]`): The checkpoint index mapping parameter name to binary checkpoint filename
+        prefix_to_test (`List[str]`): prefix to try if direct match is not found
+    Returns:
+        `Dict[str, List[Tuple]]`: A mapping from binary checkpoint filename to list of tuples of parameter names
+    Raises:
+        RuntimeError: if a parameter name is not found in the checkpoint index
+    """
     file_to_weights: Dict[str, List[Tuple]] = {}
 
     for iterator in [
