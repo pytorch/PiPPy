2777b3e237205ad2858d00c639450354a765f635
Eddy <ogolaedd@meta.com>
2023-06-20 15:35:07 -0700

    write atomically and log index filepath

--- pippy/hf/_SaveModule.py
+++ pippy/hf/_SaveModule.py
@@ -2,13 +2,57 @@ import torch.distributed as dist
 import pippy
 
 from itertools import chain
+import tempfile
 import logging
+import shutil
 import json
 import os
 
 CKPT_INDEX_JSON_FILENAME = "pytorch_model.bin.index.json"
 
 logger = logging.getLogger(__name__)
+
+
+def _copy_with_metadata(source: str, target: str) -> None:
+    """
+    Copy file with all its permisions and metadata.
+
+    Args:
+        source (str): path to source file
+        target (str): path to copy to
+    """
+    shutil.copy2(source, target)
+    st = os.stat(source)
+    os.chown(target, st.st_uid, st.st_gid)
+
+
+def _atomic_write(file_contents: str, target_file_path: str, mode="w") -> None:
+    """
+    Atomically writes `file_contents` into `target_file_path`.
+
+    Args:
+        file_contents (str): contents to write to file
+        target_file_path (str): path to write to
+        mode (str, optional): mode to write file with. Defaults to "w". Only "w" and "a" are supported.
+    """
+    temp_file = tempfile.NamedTemporaryFile(
+        delete=False, dir=os.path.dirname(target_file_path)
+    )
+    try:
+        if os.path.exists(target_file_path):
+            _copy_with_metadata(target_file_path, temp_file.name)
+        with open(temp_file.name, mode) as f:
+            f.write(file_contents)
+            f.flush()
+            os.fsync(f.fileno())
+
+        os.replace(temp_file.name, target_file_path)
+    finally:
+        if os.path.exists(temp_file.name):
+            try:
+                os.unlink(temp_file.name)
+            except:
+                pass
 
 
 def _save_index(
@@ -49,8 +93,7 @@ def _save_index(
     if not os.path.exists(checkpoint_dir):
         os.mkdir(checkpoint_dir)
 
-    with open(filepath, "w") as f:
-        f.write(json_str)
+    _atomic_write(json_str, filepath)
 
     logger.info(f"Saved index file to {filepath}")
 
