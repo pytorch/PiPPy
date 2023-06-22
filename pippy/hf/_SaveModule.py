c7d89746e6a756ed079a243c53135d1f97c1b23f
eddogola <64967909+eddogola@users.noreply.github.com>
2023-06-22 14:07:20 -0700

    Save module (#814)
    ## Description
    
    Replaced the initial mechanism of writing to file from a direct
    file.write to a more atomic write, using tempfile, to prevent data
    corruption if the data to write is large.
    
    Fixes #(issue)
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] New feature (non-breaking change which adds functionality)
    - [x] This change requires a documentation update
    
    ## Feature/Issue validation/testing
    
    Working on a test on another PR
    
    ## Checklist:
    
    - [x] Has code been commented, particularly in hard-to-understand areas?
    - [x] Have you made corresponding changes to the documentation?
    
    ---------
    
    Co-authored-by: Eddy <ogolaedd@meta.com>

--- pippy/hf/_SaveModule.py
+++ pippy/hf/_SaveModule.py
@@ -1,5 +1,9 @@
 import torch.distributed as dist
-import pippy
+from pippy.IR import Pipe
+
+from itertools import chain
+import tempfile
+import logging
 
 import json
 import os
@@ -7,8 +11,36 @@ import os
 CKPT_INDEX_JSON_FILENAME = "pytorch_model.bin.index.json"
 
 
+def _atomic_write(file_contents: str, target_file_path: str, mode="w") -> None:
+    """
+    Atomically writes `file_contents` into `target_file_path`.
+    Args:
+        file_contents (str): contents to write to file
+        target_file_path (str): path to write to
+        mode (str, optional): mode to write file with. Defaults to "w". Only "w" and "a" are supported.
+    """
+    # create tempfile as `move` ops aren't guaranteed to be atomic when between different file systems
+    temp_file = tempfile.NamedTemporaryFile(
+        delete=False,
+        dir=os.path.dirname(target_file_path),
+    )
+    try:
+        with open(temp_file.name, mode) as f:
+            f.write(file_contents)
+            # sync in-memory state with storage device
+            f.flush()
+            os.fsync(f.fileno())
+        os.replace(temp_file.name, target_file_path)
+    finally:
+        if os.path.exists(temp_file.name):
+            try:
+                os.unlink(temp_file.name)
+            except Exception:
+                raise RuntimeError(f"Failed to delete {temp_file.name}")
+
+
 def _save_index(
-    pipe: pippy.fx.GraphModule,
+    pipe: Pipe,
     ckpt_index_filename: str = CKPT_INDEX_JSON_FILENAME,
     checkpoint_dir: str = "checkpoints",
 ) -> None:
@@ -16,23 +48,32 @@ def _save_index(
     Saves index file describing location of weights in checkpoint.
 
     Args:
-        pipe (pippy.fx.GraphModule): pipeline graph module with weights to save
+        pipe (Pipe): pipeline graph module with weights to save
         ckpt_index_filename (str, optional): name of index file. Defaults to "pytorch_model.bin.index.json".
         checkpoint_dir (str, optional): directory to save checkpoint to. Defaults to "checkpoints".
     """
-    index_dict = {}
-    total_size = 0
-    index_dict["metadata"] = {"total_size": total_size}
+    index_dict = {
+        "metadata": {
+            "total_size": 0,
+        },
+        "weight_map": {},
+    }
 
     weight_map = {}
-    for idx, (submod_name, submod) in enumerate(pipe.split_gm.named_children()):
-        for param_name, _ in submod.named_parameters():
-            old_name = submod.remap_qualname(param_name)
+    for idx, (_, submod) in enumerate(pipe.split_gm.named_children()):  # type: ignore
+        # chain both params and buffers generators together
+        params_buffers = chain(
+            submod.named_parameters(), submod.named_buffers()
+        )
+        for param_name, _ in params_buffers:
+            old_name = submod.remap_qualname(param_name)  # type: ignore
 
-            binary_filename = _create_binary_filename(idx)
+            binary_filename = _get_binary_filename(idx)
             weight_map[old_name] = binary_filename
+
     index_dict["weight_map"] = weight_map
 
+    # serialize json
     json_str = json.dumps(index_dict, indent=4)
 
     filepath = os.path.join(checkpoint_dir, ckpt_index_filename)
@@ -41,11 +82,13 @@ def _save_index(
     if not os.path.exists(checkpoint_dir):
         os.mkdir(checkpoint_dir)
 
-    with open(filepath, "w") as f:
-        f.write(json_str)
+    # write index file atomically to avoid partial/corrupted writes
+    _atomic_write(json_str, filepath)
+
+    logging.info(f"Saved index file to {filepath}")
 
 
-def _create_binary_filename(cur_idx: int) -> str:
+def _get_binary_filename(cur_idx: int) -> str:  # type: ignore[valid-type]
     """
     Gets filename for pytorch checkpoint binary based on current index and world size.
 
@@ -55,7 +98,7 @@ def _create_binary_filename(cur_idx: int) -> str:
     Returns:
         str: checkpoint filename
     """
-    cur_idx = str(cur_idx + 1).zfill(5)
+    idx = str(cur_idx + 1).zfill(5)
     world_size = str(dist.get_world_size()).zfill(5)
 
-    return f"pytorch_model-{cur_idx}-of-{world_size}.bin"
+    return f"pytorch_model-{idx}-of-{world_size}.bin"
