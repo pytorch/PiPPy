70e7715c03c78f104430956ab537544d50aea35c
Eddy <ogolaedd@meta.com>
2023-06-28 16:35:14 -0700

    fix formatting errors

--- test/local_test_ckpt_index_file.py
+++ test/local_test_ckpt_index_file.py
@@ -6,19 +6,16 @@ import unittest
 from copy import deepcopy
 from typing import List
 
-from pippy.hf._SaveModule import save_checkpoint
-from pippy.IR import pipe_split, TrivialLossWrapper
-from pippy.LoadModule import load_checkpoint
-from pippy.compile import compile_stage
 import torch
 
 import torch.distributed as dist
 import torch.optim as optim
 from pippy.compile import compile_stage
 
-from pippy.hf._SaveModule import _save_checkpoint, _save_index
+from pippy.hf._SaveModule import save_checkpoint
 from pippy.IR import pipe_split, TrivialLossWrapper
 from pippy.LoadModule import load_checkpoint
+
 
 DEFAULT_FILENAME = "pytorch_model.bin.index.json"
 CKPT_DIR = "test_ckpts"
