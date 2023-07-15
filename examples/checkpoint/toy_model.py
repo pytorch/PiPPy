2021af4c870db2a601dd4282898da970194f2e00
Eddy <ogolaedd@meta.com>
2023-07-14 19:05:26 -0700

    save ckpts at the beggining of the epoch

--- examples/checkpoint/toy_model.py
+++ examples/checkpoint/toy_model.py
@@ -85,8 +85,14 @@ def run_worker(args):
     # Create an optimizer for stage submodule's parameters
     optimizer = optim.SGD(stage.submod.parameters(), lr=1e-3, momentum=0.9)
 
-    for epoch in range(2):  # change to no. epochs
+    for epoch in range(args.epochs):  # change to no. epochs
         print(f"Epoch: {epoch + 1}")
+
+        # save checkpoints
+        if (epoch + 1) % args.checkpoint_epochs == 0:
+            save_checkpoint(stage,
+                            checkpoint_dir=os.path.join("checkpoints", f"{epoch + 1}"),
+                            optimizer=optimizer)
 
         for k, dataset in datasets.items():
             epoch_correct = 0
@@ -108,9 +114,8 @@ def run_worker(args):
                         out_tensor = out['loss']
                         preds = out_tensor.argmax(-1)
                         correct = (preds == y).sum()
-                        all = len(y)
                         epoch_correct += correct.item()
-                        epoch_all += all
+                        epoch_all += len(y)
 
                         # Take an optimization step
                         optimizer.step()
@@ -123,16 +128,15 @@ def run_worker(args):
                             out = stage(x)
                         elif args.rank == args.world_size - 1:
                             out = stage(x)
-
-                            preds = out.argmax(-1)
-                            correct = (preds == y_batch).sum()
-                            all = len(y_batch)
+                            out_tensor = out['loss']
+                            preds = out_tensor.argmax(-1)
+                            correct = (preds == y).sum()
                             epoch_correct += correct.item()
-                            epoch_all += all
+                            epoch_all += len(y)
                         else:
                             stage(x)
 
-            print(f"Loader: {k}. Accuracy: {epoch_correct / epoch_all}")
+            # print(f"Loader: {k}. Accuracy: {epoch_correct / epoch_all}")
 
     dist.barrier()
     print(f"Rank {args.rank} completes")
@@ -143,7 +147,9 @@ def main(args=None):
     parser.add_argument(
         "--world_size", type=int, default=int(os.getenv("WORLD_SIZE", 4))
     )
-    parser.add_argument("--rank", type=int, default=int(os.getenv("RANK", -1)))
+    parser.add_argument(
+        "--rank", type=int, default=int(os.getenv("RANK", -1))
+    )
     parser.add_argument(
         "--master_addr", type=str, default=os.getenv("MASTER_ADDR", "localhost")
     )
@@ -154,11 +160,14 @@ def main(args=None):
         "--cuda", type=int, default=int(torch.cuda.is_available())
     )
     parser.add_argument(
-        "--chunks",
-        type=int,
-        default=4,
+        "--chunks", type=int, default=4,
     )
-    parser.add_argument("--checkpoint_epochs", type=int, default=5)
+    parser.add_argument(
+        "--epochs", type=int, default=2,
+    )
+    parser.add_argument(
+        "--checkpoint_epochs", type=int, default=5
+    )
     args = parser.parse_args(args)
 
     if args.cuda:
