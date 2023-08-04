62110396d786db35b9794f084d6df5ed6d52407e
Yeonju Ro <yro@meta.com>
2023-08-04 13:51:15 +0000

    lint

--- pippy/PipelineStage.py
+++ pippy/PipelineStage.py
@@ -321,7 +321,7 @@ class PipelineStage(torch.nn.Module):
             if user.target is operator.getitem:
                 # Recursively find the real destination
                 gi_dsts = act_send_info.setdefault(out_idx, [])
-                print(f'[Rank{self.rank}] gi_dsts{gi_dsts}')
+                print(f"[Rank{self.rank}] gi_dsts{gi_dsts}")
                 for gi_user in user.users:
                     dst_rank = self.find_dst_rank(gi_user)
                     print(
@@ -333,7 +333,7 @@ class PipelineStage(torch.nn.Module):
                 out_idx += 1
             else:
                 # In case of single output value, `out_idx` will not increase
-                print(f'[Rank{self.rank}] or do we meet here?')
+                print(f"[Rank{self.rank}] or do we meet here?")
                 dsts = act_send_info.setdefault(out_idx, [])
                 dst_rank = self.find_dst_rank(user)
                 if dst_rank is not None:
