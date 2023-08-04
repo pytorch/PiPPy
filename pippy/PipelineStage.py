6fc89a9f72b4b5793b38f7a81f7e8fb4e5439989
Yeonju Ro <yro@meta.com>
2023-08-04 13:51:15 +0000

    lint

--- pippy/PipelineStage.py
+++ pippy/PipelineStage.py
@@ -312,13 +312,13 @@ class PipelineStage(torch.nn.Module):
         if self.inner_depth == 1:
             node = self.node
         else:
-            node = self.nodes[-1] # last node result is sent
+            node = self.nodes[-1]  # last node result is sent
 
         for user in node.users:
             if user.target is operator.getitem:
                 # Recursively find the real destination
                 gi_dsts = act_send_info.setdefault(out_idx, [])
-                print(f'[Rank{self.rank}] gi_dsts{gi_dsts}')
+                print(f"[Rank{self.rank}] gi_dsts{gi_dsts}")
                 for gi_user in user.users:
                     dst_rank = self.find_dst_rank(gi_user)
                     print(
@@ -330,7 +330,7 @@ class PipelineStage(torch.nn.Module):
                 out_idx += 1
             else:
                 # In case of single output value, `out_idx` will not increase
-                print(f'[Rank{self.rank}] or do we meet here?')
+                print(f"[Rank{self.rank}] or do we meet here?")
                 dsts = act_send_info.setdefault(out_idx, [])
                 dst_rank = self.find_dst_rank(user)
                 if dst_rank is not None:
@@ -579,8 +579,6 @@ class PipelineStage(torch.nn.Module):
                             out_val = self.submods[i](out_val, **kwargs)
             else:
                 out_val = self.submod(*args, **kwargs)
-
-
         return out_val
 
     def backward_maybe_with_nosync(self, bwd_kwargs: Dict, is_last_chunk: bool):
@@ -633,7 +631,6 @@ class PipelineStage(torch.nn.Module):
             )
 
             print(f"[Rank{self.rank}] Arrived here")
-
 
         except Exception as e:
             exc_msg = f"""
@@ -709,6 +706,7 @@ class PipelineStage(torch.nn.Module):
             print(
                 f"[Rank{self.rank}] ArgsSplit: {args_split}, KwargsSplit: {kwargs_split}"
             )
+
 
         # Activation send requests of all chunk
         all_send_reqs: List[dist.Work] = []
