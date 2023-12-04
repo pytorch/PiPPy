15b95484be4f90ee4f8ee2e354f227a8ac464140
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-12-04 18:17:50 +0000

    adding logs

--- examples/inference/tp-llama/benchmark.py
+++ examples/inference/tp-llama/benchmark.py
@@ -2,10 +2,10 @@ import os
 
 import torch
 import torch.distributed as dist
-from benchmark_utils import load_model, compile_model, display_memory_stats, display_model_size, benchmark_text_completion, prepare_prompts, write_to_csv
+from benchmark_utils import load_model, compile_model, display_memory_stats, display_model_size, benchmark_text_completion, prepare_prompts, write_to_csv, read_prompt_from_file
 import logging
 import fire
-
+from torch._dynamo.utils import CompileProfiler
 # Initialize logging for PyTorch Dynamo
 torch._logging.set_logs(dynamo=logging.INFO, aot=logging.INFO, inductor=logging.INFO)
 
@@ -17,7 +17,7 @@ def main(model_args: str,
         num_trials: int = 5,
         warmup: int = 2,
         batch_size:int=1,
-        max_prompt_len: int = 100,
+        max_prompt_len: int = 10,
         max_gen_len: int = 10,
         
         # benchmark_mode: str ="text_completion", # chat_completion
@@ -44,10 +44,13 @@ def main(model_args: str,
         peak_active_gb, peak_reserved_gb = display_memory_stats("model_loading")
     
     # Compile the model
-    # compile_model(model, )
-    
+    # with CompileProfiler() as prof:
+    #     compiled_model = torch.compile(model, backend="inductor") 
+    #     print(prof.report())  
     # Run inference
-    prompt = "Generative AI has emerged as a groundbreaking domain, reshaping creative and technological landscapes. Leveraging deep learning, it can produce content, from images to text, that's often indistinguishable from human-generated work. Generative Adversarial Networks (GANs) stand out, where two neural networks contest to generate realistic outputs. OpenAI's DALL-E creates stunning visual art, while GPT models craft coherent text. Beyond art, generative AI aids in drug discovery, simulating molecular structures. However, it's not without challenges. Ethical concerns arise, especially with deepfakes and misinformation. As generative AI evolves, striking a balance between its potential and responsible use becomes paramount."
+    # prompt = "Generative AI has emerged as a groundbreaking domain, reshaping creative and technological landscapes. Leveraging deep learning, it can produce content, from images to text, that's often indistinguishable from human-generated work. Generative Adversarial Networks (GANs) stand out, where two neural networks contest to generate realistic outputs. OpenAI's DALL-E creates stunning visual art, while GPT models craft coherent text. Beyond art, generative AI aids in drug discovery, simulating molecular structures. However, it's not without challenges. Ethical concerns arise, especially with deepfakes and misinformation. As generative AI evolves, striking a balance between its potential and responsible use becomes paramount."
+    file_name = 'prompts.txt'
+    prompt = read_prompt_from_file(file_name)
     print("prompts are ready")
     batch_prompts= prepare_prompts(prompt=prompt, tokenizer=tokenizer, max_prompt_len=max_prompt_len, batch_size=batch_size)
     print(f"batched prompts are ready and size is {len(batch_prompts)} and each element size is {len(batch_prompts[0])}")
@@ -62,6 +65,8 @@ def main(model_args: str,
         peak_active_gb, peak_reserved_gb = display_memory_stats("end_of_inference")
         metrics = {}
         metrics["model_size"] = model_size
+        metrics["max_gen_len"] = max_gen_len
+        metrics["max_prompt_len"] = max_prompt_len
         metrics["batch_size"] = batch_size
         metrics["tokens/second"] = tokens_per_second
         metrics["bandwidth"] = bandwidth
