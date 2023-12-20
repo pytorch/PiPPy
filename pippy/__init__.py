ea7d1d65f28b508ee9f5db65603d88a83608e46c
Ke Wen <kw2501@meta.com>
2023-12-20 14:10:02 -0500

    Simplify llama example (#921)
    Add README.
    Remove unnecessary logics.

--- pippy/__init__.py
+++ pippy/__init__.py
@@ -9,6 +9,7 @@ from pippy.IR import (
     TrivialLossWrapper,
 )
 from pippy.ModelSplit import split_into_equal_size, split_on_size_threshold
+from pippy.PipelineStage import PipelineStage
 
 
 __all__ = [
@@ -16,6 +17,7 @@ __all__ = [
     "LossWrapper",
     "TrivialLossWrapper",
     "Pipe",
+    "PipelineStage",
     "pipe_split",
     "PipeSplitWrapper",
     "annotate_split_points",
