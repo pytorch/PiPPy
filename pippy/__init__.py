556f6607d4777c83cfeb31b85d438db3c0a2157e
Ke Wen <kw2501@meta.com>
2023-12-20 09:08:32 -0800

    Simplify llama example

--- pippy/__init__.py
+++ pippy/__init__.py
@@ -8,6 +8,7 @@ from pippy.IR import (
     PipeSplitWrapper,
     TrivialLossWrapper,
 )
+from pippy.PipelineStage import PipelineStage
 from pippy.ModelSplit import split_into_equal_size, split_on_size_threshold
 
 
@@ -16,6 +17,7 @@ __all__ = [
     "LossWrapper",
     "TrivialLossWrapper",
     "Pipe",
+    "PipelineStage",
     "pipe_split",
     "PipeSplitWrapper",
     "annotate_split_points",
