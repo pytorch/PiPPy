21e146298952793f0a3c7265ece70b363db1635d
Ke Wen <kw2501@meta.com>
2024-04-23 15:13:52 -0700

    Add comments in debug.py

--- pippy/debug.py
+++ pippy/debug.py
@@ -5,6 +5,11 @@ import os
 import torch
 
 
+# PIPPY_VERBOSITY is an environment variable that controls the logging level.
+# It can be set to one of the following:
+#   - WARNING (default)
+#   - INFO
+#   - DEBUG
 PIPPY_VERBOSITY = os.environ.get("PIPPY_VERBOSITY")
 if PIPPY_VERBOSITY not in [None, "WARNING", "INFO", "DEBUG"]:
     logging.warning(f"Unsupported PIPPY_VERBOSITY level: {PIPPY_VERBOSITY}")
@@ -18,6 +23,9 @@ if PIPPY_VERBOSITY:
 
 
 def friendly_debug_info(v):
+    """
+    Helper function to print out debug info in a friendly way.
+    """
     if isinstance(v, torch.Tensor):
         return f"Tensor({v.shape}, grad={v.requires_grad})"
     else:
@@ -25,4 +33,8 @@ def friendly_debug_info(v):
 
 
 def map_debug_info(a):
+    """
+    Helper function to apply `friendly_debug_info` to items in `a`.
+    `a` may be a list, tuple, or dict.
+    """
     return torch.fx.node.map_aggregate(a, friendly_debug_info)
